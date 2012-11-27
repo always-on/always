fd634e2c37af7043bb69f60d2e24d144e4be5096
Charles Rich <rich@wpi.edu>
2012-11-27 08:21:16 -0500

    Added Story.UI

---
+++ plugins/story-plugin/Story.UI/StoryPlugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Agent.Tcp;
+using System.Windows.Threading;
+using Agent.Core;
+using Newtonsoft.Json.Linq;
+
+namespace Story.UI
+{
+    class StoryPlugin : IPlugin
+    {
+		StoryPage story;
+        IMessageDispatcher _remote;
+        IUIThreadDispatcher _uiThreadDispatcher;
+
+        public StoryPlugin(IMessageDispatcher remote, IUIThreadDispatcher uiThreadDispatcher)
+        {
+            this._remote = remote;
+            this._uiThreadDispatcher = uiThreadDispatcher;
+            uiThreadDispatcher.BlockingInvoke(() =>
+            {
+                story = new StoryPage();
+            });
+            //
+            //_remote.RegisterReceiveHandler("calendar.display",
+			//	  new MessageHandlerDelegateWrapper(m => display(m)));
+        }
+
+		public void Dispose()
+		{
+			//_remote.RemoveReceiveHandler("calendar.display");
+		}
+
+
+
+        public System.Windows.UIElement GetUIElement()
+        {
+            return story;
+        }
+    }
+}
