8bed69d2eacda7ddb7f5e241f556e4b99cb6846d
Charles Rich <rich@wpi.edu>
2012-11-28 18:39:42 -0500

    Add spacing

--- core/agent/java/edu/wpi/always/ICollaborationManager.java
+++ core/agent/java/edu/wpi/always/ICollaborationManager.java
@@ -3,5 +3,5 @@ package edu.wpi.always;
 public interface ICollaborationManager {
 	public void start();
 
-	public void addRegistry(Registry registry);
+	public void addRegistry (Registry registry);
 }
