08583b3b03534655ece113ef50bcd49f8c33d49b
Charles Rich <rich@wpi.edu>
2012-12-24 10:30:42 -0500

    Add new rummy files

---
+++ plugins/rummy-plugin/rummy/java/edu/wpi/always/rummy/RummyProgram.java
@@ -0,0 +1,52 @@
+package edu.wpi.always.rummy;
+
+import edu.wpi.always.*;
+import edu.wpi.always.client.ClientRegistry;
+import edu.wpi.always.cm.*;
+import edu.wpi.always.cm.perceptors.dummy.DummyPerceptorsRegistry;
+import edu.wpi.always.cm.schemas.*;
+import org.picocontainer.*;
+
+public class RummyProgram {
+
+   public static void main (String[] args) {
+      final ProgramBootstrapper program = new ProgramBootstrapper(false);
+      program.addRegistry(new PicoRegistry() {
+         
+         @Override
+         public void register (MutablePicoContainer container) {
+            container.as(Characteristics.CACHE).addComponent(
+                  IRelationshipManager.class, FakeRelationshipManager.class);
+            container.as(Characteristics.CACHE).addComponent(
+                  ICollaborationManager.class,
+                  edu.wpi.always.cm.CollaborationManager.class);
+         }
+      });
+
+      program.addCMRegistry(new FakeGuiRegistry());
+      
+      program.addCMRegistry(new SchemaRegistry() {
+
+         @Override
+         public void register(SchemaManager manager) {
+            manager.registerSchema(DiscoBasedSchema.class, false);
+            manager.registerSchema(SimpleGreetingsSchema.class, true);
+            manager.registerSchema(MovementTrackerSchema.class, true);
+            //manager.registerSchema(FaceTrackerSchema.class, true);
+            manager.registerSchema(ActivityStarterSchema.class, true);
+            manager.registerSchema(RummySchema.class, false); /////////
+         }
+      });
+      
+      program.addCMRegistry(new ClientRegistry() {
+         
+         @Override
+         public void register (MutablePicoContainer container) {
+            super.register(container);
+            container.as(Characteristics.CACHE).addComponent(
+                  RummyClientPlugin.class);
+         }
+      });
+      program.start();
+   }
+}
---
+++ plugins/rummy-plugin/rummy/java/edu/wpi/always/rummy/RummySchema.java
@@ -0,0 +1,36 @@
+package edu.wpi.always.rummy;
+
+import edu.wpi.always.cm.*;
+
+public class RummySchema extends SchemaImplBase {
+
+	private final RummyClientPlugin plugin;
+	private boolean firstRun = true;
+
+	public RummySchema(BehaviorProposalReceiver behaviorReceiver,
+			BehaviorHistory resourceMonitor, RummyClientPlugin plugin) {
+		super(behaviorReceiver, resourceMonitor);
+		this.plugin = plugin;
+		
+		setNeedsFocusResouce();
+	}
+
+	@Override
+	public void run() {
+		if (firstRun) {
+			firstRun = false;
+			plugin.initInteraction();
+		}
+
+		BehaviorBuilder r = plugin.updateInteraction(lastProposalIsDone());
+
+		BehaviorMetadata m = r.getMetadata();
+		Behavior b = r.build();
+		
+		if(b.equals(Behavior.NULL) && !plugin.gameOver()) {
+			proposeIdle(0.7);
+		} else {
+			propose(b, m);
+		}
+	}
+}
