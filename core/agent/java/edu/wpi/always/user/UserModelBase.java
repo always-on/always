6e7e8353ec023e6ace893e5cb4f96f442237e08e
Candy Sidner <sidner@wpi.edu>
2013-08-18 18:06:53 +0200

    merge

--- core/agent/java/edu/wpi/always/user/UserModelBase.java
+++ core/agent/java/edu/wpi/always/user/UserModelBase.java
@@ -1,53 +1,4 @@
-<<<<<<< HEAD
-package edu.wpi.always.user;
 
-import edu.wpi.always.*;
-import edu.wpi.always.user.calendar.Calendar;
-import edu.wpi.always.user.people.PeopleManager;
-import edu.wpi.always.user.places.PlaceManager;
-
-public abstract class UserModelBase implements UserModel {
-   
-   public static void saveIf () { 
-      if ( !INHIBIT_SAVE ) Always.THIS.getUserModel().save(); 
-   }
-   
-   protected String userName;
-   
-   @Override
-   public String getUserName () {
-      return userName;
-   }
-  
-    private static final String 
-         SESSIONS = "UserSessions",
-         START_TIME = "UserStartTime",
-         CLOSENESS = "UserCloseness";
-   
-   @Override
-   public int getSessions () { return getIntProperty(SESSIONS); }
-   
-   public void nextSession () { setProperty(SESSIONS, getSessions()+1); }
-   
-   @Override
-   public long getStartTime () { return getLongProperty(START_TIME); }
-   
-   public void start () { setProperty(START_TIME, System.currentTimeMillis()); }
-
-   @Override
-   public Closeness getCloseness () {
-      if ( userName == null ) return Closeness.Stranger;  // for uninitialized model
-      String closeness = getProperty(CLOSENESS);
-      return closeness == null ? Closeness.Stranger : Closeness.valueOf(closeness);
-   }
-
-   @Override
-   public void setCloseness (Closeness closeness) { 
-      setProperty(CLOSENESS, closeness.name()); 
-   }
-  
-}
-=======
 package edu.wpi.always.user;
 
 import edu.wpi.always.*;
@@ -91,6 +42,4 @@ public abstract class UserModelBase implements UserModel {
    public void setCloseness (Closeness closeness) { 
       setProperty(CLOSENESS, closeness.name()); 
    }
-  
 }
->>>>>>> upstream/develop
