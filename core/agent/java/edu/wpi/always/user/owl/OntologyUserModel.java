b9b313c78ef91e6993bebce2d477e8fbb6a68d2a
Charles Rich <rich@wpi.edu>
2013-09-11 22:09:07 +0200

    Fix typo

--- core/agent/java/edu/wpi/always/user/owl/OntologyUserModel.java
+++ core/agent/java/edu/wpi/always/user/owl/OntologyUserModel.java
@@ -36,7 +36,7 @@ public class OntologyUserModel extends UserModelBase {
       if ( this.userName == null ) {
          this.userName = userName;
          int space = userName.indexOf(' ');
-         userFirstName = space == 0 ? userName : userName.substring(0, space);
+         userFirstName = space < 0 ? userName : userName.substring(0, space);
          this.user = ontology.getNamedIndividual(userName);
          if ( !user.hasSuperclass(OntologyPerson.USER_CLASS) ) {
             user.addSuperclass(OntologyPerson.USER_CLASS);
