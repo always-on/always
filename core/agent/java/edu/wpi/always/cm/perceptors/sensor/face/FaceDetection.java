996bebb69fd98146762e3e4965eb2e0f6c17b81f
Charles Rich <rich@wpi.edu>
2013-09-24 07:31:09 +0200

    Better Java style

--- core/agent/java/edu/wpi/always/cm/perceptors/sensor/face/FaceDetection.java
+++ core/agent/java/edu/wpi/always/cm/perceptors/sensor/face/FaceDetection.java
@@ -5,52 +5,49 @@ import edu.wpi.always.*;
 
 public class FaceDetection {
 
-   private final CPPinterface lib;
-
    public FaceDetection (boolean debug, Always.AgentType agentType) {
 
-      lib = CPPinterface.INSTANCE;
       int intDebug = debug ? 1 : 0;
-      if ( lib != null ) {
+      if ( CPPinterface.INSTANCE != null ) {
          switch (agentType) {
             case Mirror:
-               lib.initAgentShoreEngine(intDebug);
+               CPPinterface.INSTANCE.initAgentShoreEngine(intDebug);
                // fall through
             case Reeti:
                // TODO: This should come by reading user/Reeti.json
                String[] ptr = new String[] { "130.215.28.4" };
-               lib.initReetiShoreEngine(ptr, intDebug);
+               CPPinterface.INSTANCE.initReetiShoreEngine(ptr, intDebug);
                break;
             case Unity:
-               lib.initAgentShoreEngine(intDebug);
+               CPPinterface.INSTANCE.initAgentShoreEngine(intDebug);
                break;
          }
       }
    }
 
    public CPPinterface.FaceInfo getAgentFaceInfo (boolean debug) {
-      return lib == null ? new CPPinterface.FaceInfo() : 
-         lib.getAgentFaceInfo(debug ? 1 : 0);
+      return CPPinterface.INSTANCE == null ? new CPPinterface.FaceInfo() : 
+         CPPinterface.INSTANCE.getAgentFaceInfo(debug ? 1 : 0);
    }
 
    public CPPinterface.FaceInfo getReetiFaceInfo (boolean debug) {
-      return lib == null ? new CPPinterface.FaceInfo() : 
-         lib.getReetiFaceInfo(debug ? 1 : 0);
+      return CPPinterface.INSTANCE == null ? new CPPinterface.FaceInfo() : 
+         CPPinterface.INSTANCE.getReetiFaceInfo(debug ? 1 : 0);
    }
 
    public void terminateFaceDetectionProcess (boolean debug,
          Always.AgentType agentType) {
       int intDebug = debug ? 1 : 0;
-      if ( lib != null )
+      if ( CPPinterface.INSTANCE != null )
          switch (agentType) {
             case Mirror:
-               lib.terminateAgentShoreEngine(intDebug);
+               CPPinterface.INSTANCE.terminateAgentShoreEngine(intDebug);
                // fall through
             case Reeti:
-               lib.terminateReetiShoreEngine(intDebug);
+               CPPinterface.INSTANCE.terminateReetiShoreEngine(intDebug);
                break;
             case Unity:
-               lib.terminateAgentShoreEngine(intDebug);
+               CPPinterface.INSTANCE.terminateAgentShoreEngine(intDebug);
                break;
          }
    }
