89c3d71fcf1ee05492434fea4da3bebf141d89e4
lring <lring@ccs.neu.edu>
2013-10-02 11:13:33 -0400

    Fixes to merges

--- plugins/scriptbuilder-plugin/java/pluginCore/ScriptbuilderSchema.java
+++ plugins/scriptbuilder-plugin/java/pluginCore/ScriptbuilderSchema.java
@@ -1,35 +1,12 @@
 package pluginCore;
 
-<<<<<<< HEAD
-import edu.wpi.always.*;
-import edu.wpi.always.client.*;
-import edu.wpi.always.cm.primitives.FaceExpressionBehavior;
-import edu.wpi.always.cm.schemas.*;
-import edu.wpi.always.user.UserModel;
-import edu.wpi.always.user.people.PeopleManager;
-import edu.wpi.always.user.places.PlaceManager;
-=======
 import edu.wpi.always.cm.schemas.ActivityStateMachineSchema;
->>>>>>> upstream/develop
 import edu.wpi.disco.rt.ResourceMonitor;
 import edu.wpi.disco.rt.behavior.*;
 import edu.wpi.disco.rt.menu.*;
 
 public class ScriptbuilderSchema extends ActivityStateMachineSchema {
 
-<<<<<<< HEAD
-	public ScriptbuilderSchema(BehaviorProposalReceiver behaviorReceiver,
-			BehaviorHistory behaviorHistory, ResourceMonitor resourceMonitor,
-			MenuPerceptor menuPerceptor, Keyboard keyboard,
-			UIMessageDispatcher dispatcher, PlaceManager placeManager,
-			PeopleManager peopleManager,Always always) {
-		 super(new ScriptbuilderCoreScript(new RAGStateContext(
-	                keyboard, dispatcher, placeManager, peopleManager,always)),
-		            behaviorReceiver, behaviorHistory, resourceMonitor, menuPerceptor);
-	}
-
-}
-=======
    public ScriptbuilderSchema (AdjacencyPair init,
          BehaviorProposalReceiver behaviorReceiver,
 			BehaviorHistory behaviorHistory, ResourceMonitor resourceMonitor,
@@ -43,4 +20,3 @@ public class ScriptbuilderSchema extends ActivityStateMachineSchema {
       super.run();
    }
 }
->>>>>>> upstream/develop
