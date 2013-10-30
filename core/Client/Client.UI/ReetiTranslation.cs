760cc6f1c720b3e5e67168a3991dfe315b4dfb5c
Mohammad <mshayganfar@wpi.edu>
2013-10-30 16:05:24 -0700

    Reeti_Mirroring

---
+++ core/Client/Client.UI/ReetiTranslation.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agent.UI
+{
+    class ReetiTranslation
+    {
+        private ReetiCommunication reeti = new ReetiCommunication();
+        private const String HORIZONTAL = "horizontal";
+        private const String VERTICAL = "vertical";
+        private String Smooth = " smooth:";
+        private const String Head_Nod = "Global.HeadNod.play();";
+        private const String Hor_Rotate = "Global.servo.neckRotat =";
+        private const String Ver_Tilt = "Global.servo.neckTilt =";
+        private String Neutral_Position = "Global.servo.neutralPosition();"; 
+        private const String Smile = "Global.Happy.play();";
+        private const String Concern = "Global.Sad.play();";
+
+        private String ConstructMessage(double HorOutput, double VerOutput)
+        {
+            String cmd;
+            cmd = Hor_Rotate + HorOutput + Smooth + "1s" + ",";
+            cmd += Ver_Tilt;
+            cmd += VerOutput + Smooth + "1s" + ";";
+            return cmd;
+        }
+
+        private double findOutput(String HorOrVer, String cmd) // What does this method do?
+        {
+            double output;
+            int start = 0;
+            int end = 0;
+            if (HorOrVer.Equals(HORIZONTAL))
+            {
+                start = cmd.IndexOf(HORIZONTAL) + 12;
+                end = cmd.IndexOf(VERTICAL) - 2;
+            }
+            else if (HorOrVer.Equals(VERTICAL))
+            {
+                start = cmd.IndexOf(VERTICAL) + 10;
+                end = cmd.IndexOf("</bookmark>") - 1;
+            }
+            output = Convert.ToSingle(cmd.Substring(start, end - start));
+            return output;
+        }
+
+        private double mapOutput(String HorOrVer, double output)
+        {
+            if (HorOrVer.Equals(HORIZONTAL))
+            {
+                //Mirror the Agent 
+                if (output < 0)
+                {
+                    output *= -1;
+                    output = (output * 25) + 50;
+                }
+                else if (output > 0)
+                {
+                    output = (output * 25);
+                }
+                else // DO you mean output == 0?
+                    output = 50;    //Insert from JSOn File
+            }
+            else if (HorOrVer.Equals(VERTICAL))
+            {
+                if (output < 0)
+                {
+                    output *= -1;
+                    output = (output * 25);
+                }
+                else if (output > 0)
+                {
+                    output = (output * 25) + 50;
+                }
+                else
+                    output = 55.56; //Insert from json File
+            }
+            return output;
+        }
+
+        public void TranslateToReetiCommand(String task, String Command) // How about sad face?
+        {
+            if (Command.Contains("NOD") && task.Equals("perform"))
+            {
+                SendCommand(Head_Nod);
+            }
+            if (Command.Contains("GAZE") && task.Equals("speech"))
+            {
+                double HorOutput = mapOutput(HORIZONTAL, findOutput(HORIZONTAL, Command));
+                double VerOutput = mapOutput(VERTICAL, findOutput(VERTICAL, Command));
+
+                String command = ConstructMessage(HorOutput, VerOutput);
+                SendCommand(command);
+            }
+            if (Command.Contains("SMILE") && task.Equals("perform"))
+            {
+                SendCommand(Smile);
+            }
+        }
+
+        private void SendCommand(String Command)
+        {
+            reeti.Send(Command);
+        }
+    }
+}
