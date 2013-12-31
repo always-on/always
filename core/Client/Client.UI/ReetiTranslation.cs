4ea1f414180f1c1320701851a043464d0ba19e23
Mohammad <mshayganfar@wpi.edu>
2013-12-31 15:13:57 -0800

    I completed the delay part translated for Reeti.

--- core/Client/Client.UI/ReetiTranslation.cs
+++ core/Client/Client.UI/ReetiTranslation.cs
@@ -10,28 +10,20 @@ namespace Agent.UI
     {
         private ReetiCommunication reeti = new ReetiCommunication();
 
-        private const String HORIZONTAL        = "horizontal";
-        private const String VERTICAL          = "vertical";
-        private const String headNod           = "Global.SmallNod.play();";
-        private const String neckRotate        = "Global.servo.neckRotat =";
-        private const String neckTilt          = "Global.servo.neckTilt =";
-        private const String leftEyePan        = "Global.servo.leftEyePan =";
-        private const String leftEyeTilt       = "Global.servo.leftEyeTilt =";
-        private const String rightEyePan       = "Global.servo.rightEyePan =";
-        private const String rightEyeTilt      = "Global.servo.rightEyeTilt =";
-        private const String neutralPosition   = "Global.servo.neutralPosition();"; 
-        private const String smileFace         = "Global.Happy.play();";
-        private const String concernFace       = "Global.Sad.play();";
-        private const String beginSpeech       = "Global.Talk.play(0.3);";
-        private const String endSpeech         = "Global.Talk.play(0.3);";
-
-        private String ConstructMessage(double HorOutput, double VerOutput)
-        {
-            String cmd;
-            cmd  = neckRotate + HorOutput + " smooth:1s,";
-            cmd += neckTilt   + VerOutput + " smooth:1s,";
-            return cmd;
-        }
+        private const String HORIZONTAL = "horizontal";
+        private const String VERTICAL = "vertical";
+        //private const String neckRotate        = "Global.servo.neckRotat =";
+        //private const String neckTilt          = "Global.servo.neckTilt =";
+        //private const String leftEyePan        = "Global.servo.leftEyePan =";
+        //private const String leftEyeTilt       = "Global.servo.leftEyeTilt =";
+        //private const String rightEyePan       = "Global.servo.rightEyePan =";
+        //private const String rightEyeTilt      = "Global.servo.rightEyeTilt =";
+        private const String headNod = "Global.SmallNod.play();";
+        private const String neutralPosition = "Global.servo.neutralPosition();";
+        private const String smileFace = "Global.Happy.play();";
+        private const String concernFace = "Global.Sad.play();";
+        private const String beginSpeech = "Global.Talk.play(0.3);";
+        private const String endSpeech = "Global.Talk.play(0.3);";
 
         private double findOutput(String HorOrVer, String cmd)
         {
@@ -55,7 +47,6 @@ namespace Agent.UI
                     start = cmd.IndexOf(VERTICAL) + 10;
                     end = cmd.IndexOf("/>") - 2;
                 }
-
             }
             output = Convert.ToSingle(cmd.Substring(start, end - start));
             return output;
@@ -115,23 +106,7 @@ namespace Agent.UI
             }
             if (Command.Contains("DELAY"))
             {
-                StreamWriter log;
-
-                if (!File.Exists(@"C:\Users\mel\Documents\logfile.txt"))
-                {
-                    log = new StreamWriter(@"C:\Users\mel\Documents\logfile.txt");
-                }
-                else
-                {
-                    log = File.AppendText(@"C:\Users\mel\Documents\logfile.txt");
-                }
-
-                log.WriteLine(DateTime.Now);
-
-                log.WriteLine("Delay Amount: " + getDelayAmount(Command));
-                log.WriteLine();
-
-                log.Close();
+                System.Threading.Thread.Sleep(getDelayAmount(Command));
             }
 
             //TODO: check if face tracking!!!
@@ -140,19 +115,11 @@ namespace Agent.UI
                 double HorOutput = mapOutput(HORIZONTAL, findOutput(HORIZONTAL, Command));
                 double VerOutput = mapOutput(VERTICAL, findOutput(VERTICAL, Command));
 
-                //String command = ConstructMessage(HorOutput, VerOutput);
-
                 if( (VerOutput < 75) && (VerOutput > 60) ) {
-                    /*command += rightEyePan + "80 smooth:0.5s, " + rightEyeTilt + "70 smooth:0.5s, ";
-                    command += leftEyePan + "60 smooth:0.5s, " + leftEyeTilt + "70 smooth:0.5s;";*/
-
                     SendCommand("Global.LookAway.lookAwayThink();");
                 }
                 else if ( (VerOutput < 60) && (VerOutput > 20) )
                 {
-                    /*command += rightEyePan + "60 smooth:0.5s, " + rightEyeTilt + "40 smooth:0.5s, ";
-                    command += leftEyePan + "40 smooth:0.5s, " + leftEyeTilt + "42.55 smooth:0.5s;";*/
-
                     SendCommand("Global.LookAway.lookBack();");
                 }
                 else if (VerOutput < 20)
@@ -163,9 +130,6 @@ namespace Agent.UI
                 {
                     SendCommand("Global.LookAway.lookAwayAtRight();");
                 }
-
-                //SendCommand(command);
-
             }
             if (Command.Contains("CONCERN"))
             {
