3630ae304b75f1b6f827ddcbd034ac137769e495
Mohammad <mshayganfar@wpi.edu>
2014-01-15 16:39:38 -0800

    Changes to get files from main repository.

--- core/agent/java/edu/wpi/always/client/reeti/ReetiJsonConfiguration.java
+++
@@ -1,9 +0,0 @@
-package edu.wpi.always.client.reeti;
-
-// this file is a placeholder in order to set up IP message
-// to client-- CR
-public class ReetiJsonConfiguration {
-
-   public String getIP () { return "0.0.0.0"; }
-   
-}
