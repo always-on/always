2ffcd9efa620dd83910e9757134cffafbfb88c89
Mohammad <mshayganfar@wpi.edu>
2014-01-15 17:19:14 -0800

    Updated one file.

--- core/agent/java/edu/wpi/always/client/reeti/ReetiJsonConfiguration.java
+++ core/agent/java/edu/wpi/always/client/reeti/ReetiJsonConfiguration.java
@@ -1,9 +1,169 @@
 package edu.wpi.always.client.reeti;
 
-// this file is a placeholder in order to set up IP message
-// to client-- CR
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+import org.json.simple.*;
+import java.io.InputStreamReader;
+
 public class ReetiJsonConfiguration {
 
-   public String getIP () { return "0.0.0.0"; }
-   
+   private String IP;
+
+   private String name;
+
+   private double leftEar;
+
+   private double rightEar;
+
+   private double rightLC;
+
+   private double leftLC;
+
+   private double leftEyeLid;
+
+   private double rightEyeLid;
+
+   private double leftEyeTilt;
+
+   private double rightEyeTilt;
+
+   private double leftEyePan;
+
+   private double rightEyePan;
+
+   private double bottomLip;
+
+   private double topLip;
+
+   private double neckRotat;
+
+   private double neckTilt;
+
+   private double neckPan;
+
+   public String getIP () {
+      return this.IP;
+   }
+
+   public String getName () {
+      return this.name;
+   }
+
+   public double getLeftEar () {
+      return this.leftEar;
+   }
+
+   public double getRightEar () {
+      return this.rightEar;
+   }
+
+   public double getRightLC () {
+      return this.rightLC;
+   }
+
+   public double getLeftLC () {
+      return this.leftLC;
+   }
+
+   public double getLeftEyeLid () {
+      return this.leftEyeLid;
+   }
+
+   public double getRightEyeLid () {
+      return this.rightEyeLid;
+   }
+
+   public double getLeftEyeTilt () {
+      return this.leftEyeTilt;
+   }
+
+   public double getRightEyeTilt () {
+      return this.rightEyeTilt;
+   }
+
+   public double getLeftEyePan () {
+      return this.leftEyePan;
+   }
+
+   public double getRightEyePan () {
+      return this.rightEyePan;
+   }
+
+   public double getBottomLip () {
+      return this.bottomLip;
+   }
+
+   public double getTopLip () {
+      return this.topLip;
+   }
+
+   public double getNeckRotat () {
+      return this.neckRotat;
+   }
+
+   public double getNeckTilt () {
+      return this.neckTilt;
+   }
+
+   public double getNeckPan () {
+      return this.neckPan;
+   }
+
+   public ReetiJsonConfiguration () {
+
+      try {
+
+         JSONObject jsonObject = (JSONObject) JSONValue
+               .parse(new InputStreamReader(getClass().getResourceAsStream(
+                     "/Reeti.json")));
+
+         this.IP = (String) jsonObject.get("IP");
+
+         this.name = (String) jsonObject.get("name");
+
+         this.leftEar = Double.valueOf((String) jsonObject.get("leftEar"));
+
+         this.rightEar = Double.valueOf((String) jsonObject.get("rightEar"));
+
+         this.rightLC = Double.valueOf((String) jsonObject.get("rightLC"));
+
+         this.leftLC = Double.valueOf((String) jsonObject.get("leftLC"));
+
+         this.leftEyeLid = Double
+               .valueOf((String) jsonObject.get("leftEyeLid"));
+
+         this.rightEyeLid = Double.valueOf((String) jsonObject
+               .get("rightEyeLid"));
+
+         this.leftEyeTilt = Double.valueOf((String) jsonObject
+               .get("leftEyeTilt"));
+
+         this.rightEyeTilt = Double.valueOf((String) jsonObject
+               .get("rightEyeTilt"));
+
+         this.leftEyePan = Double
+               .valueOf((String) jsonObject.get("leftEyePan"));
+
+         this.rightEyePan = Double.valueOf((String) jsonObject
+               .get("rightEyePan"));
+
+         this.bottomLip = Double.valueOf((String) jsonObject.get("bottomLip"));
+
+         this.topLip = Double.valueOf((String) jsonObject.get("topLip"));
+
+         this.neckRotat = Double.valueOf((String) jsonObject.get("neckRotat"));
+
+         this.neckTilt = Double.valueOf((String) jsonObject.get("neckTilt"));
+
+         this.neckPan = Double.valueOf((String) jsonObject.get("neckPan"));
+
+      } catch (ParseException e) {
+         e.printStackTrace();
+      }
+
+   }
+
+   public static void main (String[] args) {
+      new ReetiJsonConfiguration();
+   }
+
 }
