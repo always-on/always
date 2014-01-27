f568e976691d27523b98732148cca57355b9981c
Mohammad <mshayganfar@wpi.edu>
2014-01-27 13:35:43 -0800

    Minor changes.

--- core/Client/Client.UI/ReetiCommunication.cs
+++ core/Client/Client.UI/ReetiCommunication.cs
@@ -57,10 +57,7 @@ namespace Agent.UI
             // Connect to a remote device.
             try
             {
-                // Establish the remote endpoint for the socket.
-                // The name of the 
-                // remote device is "host.contoso.com".
-                IPHostEntry ipHostInfo = Dns.GetHostEntry(Agent.Tcp.AgentControlJsonAdapter.REETI_IP); // Was "130.215.28.4" Agent.Tcp.AgentControlJsonAdapter.REETI_IP
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Agent.Tcp.AgentControlJsonAdapter.REETI_IP);
                 IPAddress ipAddress = ipHostInfo.AddressList[0];
                 IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
 
@@ -68,14 +65,6 @@ namespace Agent.UI
                 client.BeginConnect(remoteEP,
                     new AsyncCallback(ConnectCallback), client);
                 connectDone.WaitOne();
-
-                /*Send test data to the remote device.
-                Send(client,"This is a test<EOF>");
-                sendDone.WaitOne();
-
-                // Receive the response from the remote device.
-                Receive(client);
-                receiveDone.WaitOne();*/
             }
             catch (Exception e)
             {
