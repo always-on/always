f2e908cd3a14283a73cf5b1cfd766a09f67a1233
Morteza Behrooz <mbehrooz@wpi.edu>
2014-01-27 11:30:29 -0500

    Better use of Random, using shuffle with seed

--- core/sgf/java/edu/wpi/sgf/comment/CommentingManager.java
+++ core/sgf/java/edu/wpi/sgf/comment/CommentingManager.java
@@ -1,6 +1,7 @@
 package edu.wpi.sgf.comment;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import edu.wpi.sgf.logic.AnnotatedLegalMove;
@@ -10,7 +11,12 @@ public class CommentingManager {
 
    protected CommentLibraryHandler libHandler;
 
+   private static Random random;
+   private static final int RANDOM_SEED = 12345;
+   
    public CommentingManager(){
+      
+      random = new Random(RANDOM_SEED);
 
    }
 
@@ -141,30 +147,26 @@ public class CommentingManager {
    //utilities:
    public static <E> List<E> shuffleAndGetMax3(
          List<E> someObjects){
-      Random rnd = new Random();
-      rnd.setSeed(12345);
       if(someObjects.isEmpty())
          return null;
       List<E> final3 = new ArrayList<E>();
       if(someObjects.size() > 3){
-//       Collections.shuffle(someObjects);
+         Collections.shuffle(someObjects, random);
          for(int i = 0; i < 3; i ++)
             final3.add(someObjects.get(
-                  rnd.nextInt(someObjects.size())));
+                  random.nextInt(someObjects.size())));
       }
       else
          final3.addAll(someObjects);
-//      Collections.shuffle(final3);
+      Collections.shuffle(someObjects, random);
       return final3;
    }
    
    static <E> E getOneRandomlyAmong(List<E> someObjects){
       if(someObjects.isEmpty())
          return null;
-//      Collections.shuffle(someObjects);
-      Random rnd = new Random();
-      rnd.setSeed(12345);
-      return someObjects.get(rnd.
+      Collections.shuffle(someObjects, random);
+      return someObjects.get(random.
             nextInt(someObjects.size()));
    }
 
