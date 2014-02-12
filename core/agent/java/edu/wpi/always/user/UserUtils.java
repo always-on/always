ffeab2a93676f4f8b499a4942056c309c139c31d
Charles Rich <rich@wpi.edu>
2014-02-12 14:14:44 +0100

    Clean up timestamping

--- core/agent/java/edu/wpi/always/user/UserUtils.java
+++ core/agent/java/edu/wpi/always/user/UserUtils.java
@@ -81,7 +81,7 @@ public abstract class UserUtils {
       stream.println();
       stream.println("Closeness: "+model.getCloseness());
       stream.println("Sessions: "+model.getSessions());
-      stream.println("StartTime: "+new DateTime(model.getStartTime()));
+      stream.println("StartTime: "+UserUtils.formatDate());
       stream.println();
       for (Person person : model.getPeopleManager().getPeople(true)) {
          stream.print(person);
