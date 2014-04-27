2f9596327d383a4277762e0780a6a95c4b9a7e90
Charles Rich <rich@wpi.edu>
2014-04-27 23:06:49 +0200

    Update batch files

--- core/agent/bin/always-reeti.bat
+++ core/agent/bin/always-reeti.bat
@@ -3,6 +3,6 @@ taskkill /F /IM Plugins.Startup.exe
 start Plugins.Startup.exe
 cd ..\..\..\
 :always
-   java -Djava.library.path="." -jar always.jar Unity
+   java -Djava.library.path="." -jar always.jar Reeti
    echo "Restarting"
 goto always   
