4f04670c6f052e62e43c170fbc0d46017d99cd4e
Charles Rich <rich@wpi.edu>
2014-04-28 14:13:28 +0200

    Turn shore back on in dispose method.

--- plugins/scriptbuilder-plugin/java/plugins/StorytellingSchema.java
+++ plugins/scriptbuilder-plugin/java/plugins/StorytellingSchema.java
@@ -12,14 +12,26 @@ import pluginCore.*;
 
 public class StorytellingSchema extends ScriptbuilderSchema {
 
+   private final ShoreFacePerceptor shore;
+   
 	public StorytellingSchema (BehaviorProposalReceiver behaviorReceiver,
 			BehaviorHistory behaviorHistory, ResourceMonitor resourceMonitor,
 			MenuPerceptor menuPerceptor, Keyboard keyboard,
 			UIMessageDispatcher dispatcher, PlaceManager placeManager,
 			PeopleManager peopleManager, Always always, ShoreFacePerceptor shore) {
 		 super(new ScriptbuilderCoreScript(new RAGStateContext(
-	               keyboard, dispatcher, placeManager, peopleManager, always, shore,"Storytelling")),
+	               keyboard, dispatcher, placeManager, peopleManager, always, 
+	               shore instanceof ShoreFacePerceptor.Reeti ? null : shore,
+	               "Storytelling")),
 		       behaviorReceiver, behaviorHistory, resourceMonitor, menuPerceptor, dispatcher);
+		 this.shore = shore;
 	}
-
+	
+	@Override
+   public void dispose () { 
+      super.dispose();
+      // this is here so it is run even if schema throws an error
+      if ( shore != null ) shore.start(); 
+   }
+ 
 }
