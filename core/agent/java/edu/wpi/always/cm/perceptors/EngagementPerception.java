6e87f063985767930c7f545a4da079211c4014f2
Charles Rich <rich@wpi.edu>
2014-05-08 14:06:05 +0200

    Prevent engagement action too often

--- core/agent/java/edu/wpi/always/cm/perceptors/EngagementPerception.java
+++ core/agent/java/edu/wpi/always/cm/perceptors/EngagementPerception.java
@@ -10,21 +10,27 @@ import edu.wpi.disco.rt.perceptor.Perception;
 public class EngagementPerception extends Perception {
 
    public static long IDLE_FACE_TIME = 1000; // idle->attention after seeing (far) face for this long
-   public static long ATTENTION_NO_FACE_TIMEOUT = 10000;  // attention->idle if no face for this long
+
+   public static long ATTENTION_NO_FACE_TIMEOUT = 60000;  // attention->idle if no face for this long
    
    public static long ATTENTION_FACE_TIME = 20000; // attention->initiation after seeing (far) face for this long
-   
+
    public static long INITIATION_NOT_NEAR_TIMEOUT = 20000; // initiation->idle if no near face for this long
+
+   // Note these two times (5 minutes) are to prevent the agent from performing enter attention 
+   // or initiation actions (i.e., setAgentVisible or saying "Hi") too often
+   public static long ATTENTION_TIME = 300000; // attention->idle minimum time in attention
+   public static long INITIATION_TIME = 300000; // initiation->idle minimum time in initiation
    
    // Note these two timeouts should be larger than @link{MenuTurnStateMachine#TIMEOUT_DELAY} to let the agent
    // repeat once first if it is waiting for menu response.  However, this timeout is *not* restarted
    // when agent repeats
    public static long ENGAGED_NOT_NEAR_TIMEOUT = 60000; // engaged->recovering if no near face for this long   
-   public static long ENGAGED_NO_TOUCH_TIMEOUT = 60000; //   and no touch for this long (and not moving)
+   public static long ENGAGED_NO_TOUCH_TIMEOUT = 60000; //   and no touch for this long (and no movement)
 
    // TODO: Increase these a *lot*
    public static long RECOVERING_NOT_NEAR_TIMEOUT = 60000; // engaged->idle if no near face for this long 
-   public static long RECOVERING_NO_TOUCH_TIMEOUT = 60000; //   and no touch for this long (and not moving) 
+   public static long RECOVERING_NO_TOUCH_TIMEOUT = 60000; //   and no touch for this long (and no movement) 
    
    private final EngagementState state;
 
@@ -46,8 +52,8 @@ public class EngagementPerception extends Perception {
 
          @Override
          EngagementState nextState (MovementTransition lastMovementChange,
-               FaceTransition lastFaceChange,
-               TouchTransition lastTouch, boolean hadTouch, long timeInState) {
+               FaceTransition lastFaceChange, TouchTransition lastTouch, 
+               boolean hadTouch, long timeInState) {
             if ( hadTouch ) return Engaged;
             if ( Always.isLogin() ) return Idle; // user must touch Hello
             if ( lastFaceChange.isNear ) return Initiation;
@@ -61,11 +67,12 @@ public class EngagementPerception extends Perception {
 
          @Override
          EngagementState nextState (MovementTransition lastMovementChange,
-               FaceTransition lastFaceChange,
-               TouchTransition lastTouch, boolean hadTouch, long timeInState) {
+               FaceTransition lastFaceChange, TouchTransition lastTouch, 
+               boolean hadTouch, long timeInState) {
             if ( hadTouch ) return Engaged;
             if ( lastFaceChange.isNear ) return Initiation;
-            if ( !lastFaceChange.isFace && lastFaceChange.timeSinceChange() > ATTENTION_NO_FACE_TIMEOUT
+            if ( timeInState > ATTENTION_TIME 
+                 && !lastFaceChange.isFace && lastFaceChange.timeSinceChange() > ATTENTION_NO_FACE_TIMEOUT
                  && !lastMovementChange.isMoving )
                return Idle;
             if ( lastFaceChange.isFace && lastFaceChange.timeSinceChange() > ATTENTION_FACE_TIME )
@@ -77,10 +84,11 @@ public class EngagementPerception extends Perception {
 
          @Override
          EngagementState nextState (MovementTransition lastMovementChange,
-               FaceTransition lastFaceChange,
-               TouchTransition lastTouch, boolean hadTouch, long timeInState) {
+               FaceTransition lastFaceChange, TouchTransition lastTouch, 
+               boolean hadTouch, long timeInState) {
             if ( hadTouch ) return Engaged;
-            if ( !lastFaceChange.isNear && lastFaceChange.timeSinceChange() > INITIATION_NOT_NEAR_TIMEOUT )
+            if ( timeInState > INITIATION_TIME
+                 && !lastFaceChange.isNear && lastFaceChange.timeSinceChange() > INITIATION_NOT_NEAR_TIMEOUT )
                return Idle;
             return Initiation;
          }
@@ -89,8 +97,8 @@ public class EngagementPerception extends Perception {
 
          @Override
          EngagementState nextState (MovementTransition lastMovementChange,
-               FaceTransition lastFaceChange,
-               TouchTransition lastTouch, boolean hadTouch, long timeInState) {
+               FaceTransition lastFaceChange, TouchTransition lastTouch, 
+               boolean hadTouch, long timeInState) {
             if ( hadTouch ) return Engaged;
             if ( (!lastFaceChange.isNear && lastFaceChange.timeSinceChange() > ENGAGED_NOT_NEAR_TIMEOUT)
                  && lastTouch.timeSinceChange() > ENGAGED_NO_TOUCH_TIMEOUT 
@@ -104,8 +112,8 @@ public class EngagementPerception extends Perception {
 
          @Override
          EngagementState nextState (MovementTransition lastMovementChange,
-               FaceTransition lastFaceChange,
-               TouchTransition lastTouch, boolean hadTouch, long timeInState) {
+               FaceTransition lastFaceChange, TouchTransition lastTouch, 
+               boolean hadTouch, long timeInState) {
             if ( hadTouch ) return Engaged;
             if ( timeInState > RECOVERING_NO_TOUCH_TIMEOUT 
                  && !lastFaceChange.isNear && lastFaceChange.timeSinceChange() > RECOVERING_NOT_NEAR_TIMEOUT 
@@ -115,7 +123,7 @@ public class EngagementPerception extends Perception {
          }
       };
       
-        /**
+      /**
        * Called by the engagement perceptor to figure out what state it should
        * transition to
        * 
@@ -127,10 +135,11 @@ public class EngagementPerception extends Perception {
        *           change, will be marked as not having a touch)
        * @param hadTouch true if there was a touch since the most recent update
        *           of the perceptor
+       * @param timeInState how long since first entered this state
        * @return the next state (or the current state to stay in the same state)
        */
       abstract EngagementState nextState (MovementTransition lastMovementChange,
-            FaceTransition lastFaceChange,
-            TouchTransition lastTouch, boolean hadTouch, long timeInState);
+            FaceTransition lastFaceChange, TouchTransition lastTouch, 
+            boolean hadTouch, long timeInState);
    }
 }
