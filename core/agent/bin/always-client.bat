d9467a3ef29f54734fa3e9b05e64208e20e5eb63
Charles Rich <rich@wpi.edu>
2014-05-08 21:09:23 +0200

    put back taskkill

--- core/agent/bin/always-client.bat
+++ core/agent/bin/always-client.bat
@@ -1,6 +1,7 @@
 cd C:\Dropbox\release\plugins\Plugins.Startup\bin
 ..\..\..\bin\nircmdc screensaver
 :always
+   taskkill /F /IM Plugins.Startup.exe
    Plugins.Startup.exe
    echo RESTARTING CLIENT...
 goto always   
