a41d64391e7ab01c95facde0b38d3ba6ade267cb
Charles Rich <rich@wpi.edu>
2014-06-18 18:36:19 +0200

    Catch weather errors and exit

--- plugins/weather-plugin/weather/java/edu/wpi/always/weather/wunderground/WundergroundParser.java
+++ plugins/weather-plugin/weather/java/edu/wpi/always/weather/wunderground/WundergroundParser.java
@@ -18,20 +18,23 @@ public class WundergroundParser {
     *           zip: 5-digits (default 02115)<br>
     */
    public static void main (String[] args) {
-      if ( args.length > 0 ) {
-         if ( !args[0].matches("\\d{5}((-)?\\d{4})?") )
-               throw new IllegalArgumentException("Invalid zip code: "+args[0]);
-         ZIP = args[0];
-      }
-      Always always = Always.make(null, null, null);
       String file =  UserUtils.USER_DIR+"/weatherData/"+UserUtils.formatDate()+".json";
       try (FileWriter writer = new FileWriter(file)) {
+         if ( args.length > 0 ) {
+            if ( !args[0].matches("\\d{5}((-)?\\d{4})?") )
+               throw new IllegalArgumentException("Invalid zip code: "+args[0]);
+            ZIP = args[0];
+         }
+         Always always = Always.make(null, null, null);
          WundergroundJSON weather = new WundergroundJSON(ZIP, always.getUserModel());
          // make easier to read for debugging
          Gson gson = new GsonBuilder().setPrettyPrinting().create();
          String json = gson.toJson(weather);
          writer.write(json);
          System.out.println("File written: "+file);
-      } catch (Exception e) { throw new RuntimeException(e); }
+      } catch (Exception e) { 
+         e.printStackTrace();
+         Always.exit(-1); // so goes on
+      }
    }
  }
