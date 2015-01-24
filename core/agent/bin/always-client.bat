7282b243540ff52ef3ea8fcb4191260fa60c36d8
Charles Rich <rich@wpi.edu>
2015-01-24 11:36:47 -0500

    Check/wait for hangout connection

--- core/agent/bin/always-client.bat
+++ core/agent/bin/always-client.bat
@@ -3,6 +3,8 @@ cd C:\Dropbox\%COMPUTERNAME%\release\plugins\Plugins.Startup\bin
 :always
    tasklist /FI "IMAGENAME eq Plugins.Startup.exe" 2>NUL | find /I /N "Plugins.Startup.exe">NUL
    if "%ERRORLEVEL%"=="0" taskkill /F /IM Plugins.Startup.exe
+   ping -n 1 hangout.google.com
+   if errorlevel 1 timeout /T 180
    Plugins.Startup.exe
    echo RESTARTING CLIENT...
 goto always   
